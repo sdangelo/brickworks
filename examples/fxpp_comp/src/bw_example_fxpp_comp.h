dccc39907e8443efcadf10e92d138c53a0fa24ee
Stefano D'Angelo <stefano.dangelo@orastron.com>
2023-06-25 11:38:01 +0200

    bwpp_comp + fxpp_comp

---
+++ examples/fxpp_comp/src/bw_example_fxpp_comp.h
@@ -0,0 +1,57 @@
+/*
+ * Brickworks
+ *
+ * Copyright (C) 2023 Orastron Srl unipersonale
+ *
+ * Brickworks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * Brickworks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Brickworks.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * File author: Stefano D'Angelo
+ */
+
+#ifndef _BW_EXAMPLE_FXPP_COMP_H
+#define _BW_EXAMPLE_FXPP_COMP_H
+
+#include <bwpp_comp.h>
+
+using namespace Brickworks;
+
+extern "C" {
+
+enum {
+	p_thresh,
+	p_ratio,
+	p_attack,
+	p_release,
+	p_gain,
+	p_n
+};
+
+struct _bw_example_fxpp_comp {
+	// Sub-components
+	Comp<1>	comp;
+
+	// Parameters
+	float	params[p_n];
+};
+typedef struct _bw_example_fxpp_comp bw_example_fxpp_comp;
+
+void bw_example_fxpp_comp_init(bw_example_fxpp_comp *instance);
+void bw_example_fxpp_comp_set_sample_rate(bw_example_fxpp_comp *instance, float sample_rate);
+void bw_example_fxpp_comp_reset(bw_example_fxpp_comp *instance);
+void bw_example_fxpp_comp_process(bw_example_fxpp_comp *instance, const float** x, float** y, int n_samples);
+void bw_example_fxpp_comp_set_parameter(bw_example_fxpp_comp *instance, int index, float value);
+float bw_example_fxpp_comp_get_parameter(bw_example_fxpp_comp *instance, int index);
+
+}
+
+#endif
---
+++ include/bwpp_comp.h
@@ -0,0 +1,119 @@
+/*
+ * Brickworks
+ *
+ * Copyright (C) 2023 Orastron Srl unipersonale
+ *
+ * Brickworks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * Brickworks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Brickworks.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * File author: Stefano D'Angelo
+ */
+
+#ifndef BWPP_COMP_H
+#define BWPP_COMP_H
+
+#include <bw_comp.h>
+#include <array>
+
+namespace Brickworks {
+	template<BW_SIZE_T N_CHANNELS>
+	class Comp {
+	public:
+		Comp();
+
+		void setSampleRate(float sampleRate);
+		void reset();
+		void process(
+			std::array<const float *, N_CHANNELS> x,
+			std::array<const float *, N_CHANNELS> xSC,
+			std::array<float *, N_CHANNELS> y,
+			int nSamples);
+
+		void setTreshLin(float value);
+		void setTreshDBFS(float value);
+		void setRatio(float value);
+		void setAttackTau(float value);
+		void setReleaseTau(float value);
+		void setGainLin(float value);
+		void setGainDB(float value);
+
+	private:
+		bw_comp_coeffs	 coeffs;
+		bw_comp_state	 states[N_CHANNELS];
+		bw_comp_state	*statesP[N_CHANNELS];
+	};
+	
+	template<BW_SIZE_T N_CHANNELS>
+	Comp<N_CHANNELS>::Comp() {
+		bw_comp_init(&coeffs);
+		for (unsigned int i = 0; i < N_CHANNELS; i++)
+			statesP[i] = states + i;
+	}
+	
+	template<BW_SIZE_T N_CHANNELS>
+	void Comp<N_CHANNELS>::setSampleRate(float sampleRate) {
+		bw_comp_set_sample_rate(&coeffs, sampleRate);
+	}
+	
+	template<BW_SIZE_T N_CHANNELS>
+	void Comp<N_CHANNELS>::reset() {
+		bw_comp_reset_coeffs(&coeffs);
+		for (unsigned int i = 0; i < N_CHANNELS; i++)
+			bw_comp_reset_state(&coeffs, states + i);
+	}
+	
+	template<BW_SIZE_T N_CHANNELS>
+	void Comp<N_CHANNELS>::process(
+			std::array<const float *, N_CHANNELS> x,
+			std::array<const float *, N_CHANNELS> xSC,
+			std::array<float *, N_CHANNELS> y,
+			int nSamples) {
+		bw_comp_process_multi(&coeffs, statesP, x.data(), xSC.data(), y.data(), N_CHANNELS, nSamples);
+	}
+	
+	template<BW_SIZE_T N_CHANNELS>
+	void Comp<N_CHANNELS>::setTreshLin(float value) {
+		bw_comp_set_thresh_lin(&coeffs, value);
+	}
+	
+	template<BW_SIZE_T N_CHANNELS>
+	void Comp<N_CHANNELS>::setTreshDBFS(float value) {
+		bw_comp_set_thresh_dBFS(&coeffs, value);
+	}
+	
+	template<BW_SIZE_T N_CHANNELS>
+	void Comp<N_CHANNELS>::setRatio(float value) {
+		bw_comp_set_ratio(&coeffs, value);
+	}
+	
+	template<BW_SIZE_T N_CHANNELS>
+	void Comp<N_CHANNELS>::setAttackTau(float value) {
+		bw_comp_set_attack_tau(&coeffs, value);
+	}
+	
+	template<BW_SIZE_T N_CHANNELS>
+	void Comp<N_CHANNELS>::setReleaseTau(float value) {
+		bw_comp_set_release_tau(&coeffs, value);
+	}
+	
+	template<BW_SIZE_T N_CHANNELS>
+	void Comp<N_CHANNELS>::setGainLin(float value) {
+		bw_comp_set_gain_lin(&coeffs, value);
+	}
+	
+	template<BW_SIZE_T N_CHANNELS>
+	void Comp<N_CHANNELS>::setGainDB(float value) {
+		bw_comp_set_gain_dB(&coeffs, value);
+	}
+}
+
+#endif

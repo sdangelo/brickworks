146904938bbeb80394720fd70fe95501e905929c
Stefano D'Angelo <s@dangelo.audio>
2023-07-01 11:39:11 +0200

    fxpp_vibrato

---
+++ examples/fxpp_vibrato/vst3/config_vst3.h
@@ -0,0 +1,36 @@
+/*
+ * Brickworks
+ *
+ * Copyright (C) 2023 Orastron Srl unipersonale
+ *
+ * Brickworks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * Brickworks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Brickworks.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * File authors: Stefano D'Angelo, Paolo Marrone
+ */
+
+#ifndef _VST3_CONFIG_H
+#define _VST3_CONFIG_H
+
+#define PLUGIN_SUBCATEGORY	"Fx|Modulation"
+
+#define PLUGIN_GUID_1		0x64cde9d9
+#define PLUGIN_GUID_2		0x3afe4d79
+#define PLUGIN_GUID_3		0x96d61599
+#define PLUGIN_GUID_4		0xbe1fdfe2
+
+#define CTRL_GUID_1		0xbdb79309
+#define CTRL_GUID_2		0x267049a6
+#define CTRL_GUID_3		0xb1ae13f9
+#define CTRL_GUID_4		0xd371e2c0
+
+#endif

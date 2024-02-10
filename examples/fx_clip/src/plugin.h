9685ac9b4b6cc94d9f88aa3a5a08c25539248792
Stefano D'Angelo <stefano.dangelo@orastron.com>
2024-02-10 11:45:04 +0100

    fx(pp)_clip

---
+++ examples/fx_clip/src/plugin.h
@@ -0,0 +1,76 @@
+#include "common.h"
+#include <bw_clip.h>
+#include <bw_src_int.h>
+
+#define BUF_SIZE	32
+
+typedef struct plugin {
+	bw_clip_coeffs		clip_coeffs;
+	bw_clip_state		clip_state;
+	bw_src_int_coeffs	src_up_coeffs;
+	bw_src_int_state	src_up_state;
+	bw_src_int_coeffs	src_down_coeffs;
+	bw_src_int_state	src_down_state;
+
+	float			buf[BUF_SIZE];
+} plugin;
+
+static void plugin_init(plugin *instance) {
+	bw_clip_init(&instance->clip_coeffs);
+	bw_src_int_init(&instance->src_up_coeffs, 2);
+	bw_src_int_init(&instance->src_down_coeffs, -2);
+	bw_clip_set_gain_compensation(&instance->clip_coeffs, 1);
+}
+
+static void plugin_fini(plugin *instance) {
+	(void)instance;
+}
+
+static void plugin_set_sample_rate(plugin *instance, float sample_rate) {
+	bw_clip_set_sample_rate(&instance->clip_coeffs, 2.f * sample_rate);
+}
+
+static size_t plugin_mem_req(plugin *instance) {
+	(void)instance;
+	return 0;
+}
+
+static void plugin_mem_set(plugin *instance, void *mem) {
+	(void)instance;
+	(void)mem;
+}
+
+static void plugin_reset(plugin *instance) {
+	bw_clip_reset_coeffs(&instance->clip_coeffs);
+	bw_clip_reset_state(&instance->clip_coeffs, &instance->clip_state, 0.f);
+	bw_src_int_reset_state(&instance->src_up_coeffs, &instance->src_up_state, 0.f);
+	bw_src_int_reset_state(&instance->src_down_coeffs, &instance->src_down_state, 0.f);
+}
+
+static void plugin_set_parameter(plugin *instance, size_t index, float value) {
+	switch (index) {
+	case 0:
+		bw_clip_set_gain(&instance->clip_coeffs, value);
+		break;
+	case 1:
+		bw_clip_set_bias(&instance->clip_coeffs, value);
+		break;
+	}
+}
+
+static float plugin_get_parameter(plugin *instance, size_t index) {
+	(void)instance;
+	(void)index;
+	return 0.f;
+}
+
+static void plugin_process(plugin *instance, const float **inputs, float **outputs, size_t n_samples) {
+	size_t i = 0;
+	while (i < n_samples) {
+		int n = bw_mini32(n_samples - i, BUF_SIZE >> 1);
+		bw_src_int_process(&instance->src_up_coeffs, &instance->src_up_state, inputs[0] + i, instance->buf, n);
+		bw_clip_process(&instance->clip_coeffs, &instance->clip_state, instance->buf, instance->buf, n << 1);
+		bw_src_int_process(&instance->src_down_coeffs, &instance->src_down_state, instance->buf, outputs[0] + i, n << 1);
+		i += n;
+	}
+}
--- examples/tibia_gen.sh
+++ examples/tibia_gen.sh
@@ -33,7 +33,7 @@ if [ "$1" = "common" ]; then
 fi
 
 if [ -z "$1" ]; then
-	dirs="fx_ap1 fx_ap2 fx_balance fx_bitcrush fx_cab fx_chorus fx_comp synth_simple fxpp_ap1 fxpp_ap2 fxpp_balance fxpp_bitcrush fxpp_cab fxpp_chorus fxpp_comp synthpp_simple"
+	dirs="fx_ap1 fx_ap2 fx_balance fx_bitcrush fx_cab fx_chorus fx_clip fx_comp synth_simple fxpp_ap1 fxpp_ap2 fxpp_balance fxpp_bitcrush fxpp_cab fxpp_chorus fxpp_clip fxpp_comp synthpp_simple"
 else
 	dirs="$1"
 fi

d17387f3771149adb93db927baedfd4974f57c7e
Stefano D'Angelo <stefano.dangelo@orastron.com>
2024-02-14 05:18:42 +0100

    fx(pp)_noise_gate

---
+++ examples/fx_noise_gate/src/plugin.h
@@ -0,0 +1,67 @@
+#include "common.h"
+#include <bw_noise_gate.h>
+
+typedef struct plugin {
+	bw_noise_gate_coeffs	noise_gate_coeffs;
+	bw_noise_gate_state	noise_gate_state;
+	char			ext_sidechain;
+} plugin;
+
+static void plugin_init(plugin *instance) {
+	bw_noise_gate_init(&instance->noise_gate_coeffs);
+}
+
+static void plugin_fini(plugin *instance) {
+	(void)instance;
+}
+
+static void plugin_set_sample_rate(plugin *instance, float sample_rate) {
+	bw_noise_gate_set_sample_rate(&instance->noise_gate_coeffs, sample_rate);
+}
+
+static size_t plugin_mem_req(plugin *instance) {
+	(void)instance;
+	return 0;
+}
+
+static void plugin_mem_set(plugin *instance, void *mem) {
+	(void)instance;
+	(void)mem;
+}
+
+static void plugin_reset(plugin *instance) {
+	bw_noise_gate_reset_coeffs(&instance->noise_gate_coeffs);
+	bw_noise_gate_reset_state(&instance->noise_gate_coeffs, &instance->noise_gate_state, 0.f, 0.f);
+}
+
+static void plugin_set_parameter(plugin *instance, size_t index, float value) {
+	switch (index) {
+	case 0:
+		bw_noise_gate_set_thresh_dBFS(&instance->noise_gate_coeffs, value);
+		break;
+	case 1:
+		bw_noise_gate_set_ratio(&instance->noise_gate_coeffs, value);
+		break;
+	case 2:
+		// using rise time 10% -> 90%: tau = rise time / log(9)
+		bw_noise_gate_set_attack_tau(&instance->noise_gate_coeffs, (0.001f * 0.4551196133134186f) * value);
+		break;
+	case 3:
+		// as before
+		bw_noise_gate_set_release_tau(&instance->noise_gate_coeffs, (0.001f * 0.4551196133134186f) * value);
+		break;
+	case 4:
+		instance->ext_sidechain = (char)value;
+		break;
+	}
+}
+
+static float plugin_get_parameter(plugin *instance, size_t index) {
+	(void)instance;
+	(void)index;
+	return 0.f;
+}
+
+static void plugin_process(plugin *instance, const float **inputs, float **outputs, size_t n_samples) {
+	bw_noise_gate_process(&instance->noise_gate_coeffs, &instance->noise_gate_state, inputs[0], instance->ext_sidechain ? inputs[1] : inputs[0], outputs[0], n_samples);
+}
---
+++ examples/fxpp_noise_gate/src/impl.cpp
@@ -0,0 +1,77 @@
+#include "impl.h"
+
+#include "common.h"
+#include <bw_noise_gate.h>
+
+using namespace Brickworks;
+
+class Engine {
+public:
+	NoiseGate<1>	noise_gate;
+	bool		extSidechain;
+};
+
+extern "C" {
+
+impl impl_new(void) {
+	Engine *instance = new Engine();
+	return reinterpret_cast<impl>(instance);
+}
+
+void impl_free(impl handle) {
+	Engine *instance = reinterpret_cast<Engine *>(handle);
+	delete instance;
+}
+
+void impl_set_sample_rate(impl handle, float sample_rate) {
+	Engine *instance = reinterpret_cast<Engine *>(handle);
+	instance->noise_gate.setSampleRate(sample_rate);
+}
+
+void impl_reset(impl handle) {
+	Engine *instance = reinterpret_cast<Engine *>(handle);
+	instance->noise_gate.reset();
+}
+
+void impl_set_parameter(impl handle, size_t index, float value) {
+	Engine *instance = reinterpret_cast<Engine *>(handle);
+	switch (index) {
+	case 0:
+		instance->noise_gate.setThreshDBFS(value);
+		break;
+	case 1:
+		instance->noise_gate.setRatio(value);
+		break;
+	case 2:
+		// using rise time 10% -> 90%: tau = rise time / log(9)
+		instance->noise_gate.setAttackTau((0.001f * 0.4551196133134186f) * value);
+		break;
+	case 3:
+		// as before
+		instance->noise_gate.setReleaseTau((0.001f * 0.4551196133134186f) * value);
+		break;
+	case 4:
+		instance->extSidechain = value >= 0.5f;
+		break;
+	}
+}
+
+float impl_get_parameter(impl handle, size_t index) {
+	(void)handle;
+	(void)index;
+	return 0.f;
+}
+
+void impl_process(impl handle, const float **inputs, float **outputs, size_t n_samples) {
+	Engine *instance = reinterpret_cast<Engine *>(handle);
+#ifdef WASM
+	const float *x[1] = {inputs[0]};
+	const float *xSc[1] = {instance->extSidechain ? inputs[1] : inputs[0]};
+	float *y[1] = {outputs[0]};
+	instance->noise_gate.process(x, xSc, y, n_samples);
+#else
+	instance->noise_gate.process({inputs[0]}, {instance->extSidechain ? inputs[1] : inputs[0]}, {outputs[0]}, n_samples);
+#endif
+}
+
+}

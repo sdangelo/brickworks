eff4dab96884e12fbfde0782bee8550af80caaff
Stefano D'Angelo <s@dangelo.audio>
2024-02-15 15:20:26 +0100

    fx(pp)_pan

---
+++ examples/fx_pan/src/plugin.h
@@ -0,0 +1,65 @@
+#include "common.h"
+#include <bw_pan.h>
+#include <bw_ppm.h>
+
+typedef struct plugin {
+	bw_pan_coeffs	pan_coeffs;
+	bw_ppm_coeffs	ppm_coeffs;
+	bw_ppm_state	ppm_l_state;
+	bw_ppm_state	ppm_r_state;
+} plugin;
+
+static void plugin_init(plugin *instance) {
+	bw_pan_init(&instance->pan_coeffs);
+	bw_ppm_init(&instance->ppm_coeffs);
+}
+
+static void plugin_fini(plugin *instance) {
+	(void)instance;
+}
+
+static void plugin_set_sample_rate(plugin *instance, float sample_rate) {
+	bw_pan_set_sample_rate(&instance->pan_coeffs, sample_rate);
+	bw_ppm_set_sample_rate(&instance->ppm_coeffs, sample_rate);
+}
+
+static size_t plugin_mem_req(plugin *instance) {
+	(void)instance;
+	return 0;
+}
+
+static void plugin_mem_set(plugin *instance, void *mem) {
+	(void)instance;
+	(void)mem;
+}
+
+static void plugin_reset(plugin *instance) {
+	bw_pan_reset_coeffs(&instance->pan_coeffs);
+	bw_ppm_reset_coeffs(&instance->ppm_coeffs);
+	bw_ppm_reset_state(&instance->ppm_coeffs, &instance->ppm_l_state, 0.f);
+	bw_ppm_reset_state(&instance->ppm_coeffs, &instance->ppm_r_state, 0.f);
+}
+
+static void plugin_set_parameter(plugin *instance, size_t index, float value) {
+	(void)index;
+	bw_pan_set_pan(&instance->pan_coeffs, 0.01f * value);
+}
+
+static float plugin_get_parameter(plugin *instance, size_t index) {
+	float v = 0.f;
+	switch (index) {
+	case 1:
+		v = bw_ppm_get_y_z1(&instance->ppm_l_state);
+		break;
+	case 2:
+		v = bw_ppm_get_y_z1(&instance->ppm_r_state);
+		break;
+	}
+	return bw_clipf(v, -60.f, 0.f);
+}
+
+static void plugin_process(plugin *instance, const float **inputs, float **outputs, size_t n_samples) {
+	bw_pan_process(&instance->pan_coeffs, inputs[0], outputs[0], outputs[1], n_samples);
+	bw_ppm_state *ppm_states[2] = { &instance->ppm_l_state, &instance->ppm_r_state };
+	bw_ppm_process_multi(&instance->ppm_coeffs, ppm_states, (const float **)outputs, NULL, 2, n_samples);
+}
---
+++ examples/fxpp_pan/src/impl.cpp
@@ -0,0 +1,64 @@
+#include "impl.h"
+
+#include "common.h"
+#include <bw_pan.h>
+#include <bw_ppm.h>
+
+using namespace Brickworks;
+
+class Engine {
+public:
+	Pan<1>	pan;
+	PPM<2>	ppm;
+};
+
+extern "C" {
+
+impl impl_new(void) {
+	Engine *instance = new Engine();
+	return reinterpret_cast<impl>(instance);
+}
+
+void impl_free(impl handle) {
+	Engine *instance = reinterpret_cast<Engine *>(handle);
+	delete instance;
+}
+
+void impl_set_sample_rate(impl handle, float sample_rate) {
+	Engine *instance = reinterpret_cast<Engine *>(handle);
+	instance->pan.setSampleRate(sample_rate);
+	instance->ppm.setSampleRate(sample_rate);
+}
+
+void impl_reset(impl handle) {
+	Engine *instance = reinterpret_cast<Engine *>(handle);
+	instance->pan.reset();
+	instance->ppm.reset();
+}
+
+void impl_set_parameter(impl handle, size_t index, float value) {
+	(void)index;
+	Engine *instance = reinterpret_cast<Engine *>(handle);
+	instance->pan.setPan(0.01f * value);
+}
+
+float impl_get_parameter(impl handle, size_t index) {
+	Engine *instance = reinterpret_cast<Engine *>(handle);
+	float v = instance->ppm.getYZ1(index - 1);
+	return v < -60.f ? -60.f : (v > 0.f ? 0.f : v);
+}
+
+void impl_process(impl handle, const float **inputs, float **outputs, size_t n_samples) {
+	Engine *instance = reinterpret_cast<Engine *>(handle);
+#ifdef WASM
+	float *yL[1] = {outputs[0]};
+	float *yR[1] = {outputs[1]};
+	instance->pan.process(inputs, yL, yR, n_samples);
+	instance->ppm.process(outputs, nullptr, n_samples);
+#else
+	instance->pan.process({inputs[0]}, {inputs[1]}, {outputs[0]}, {outputs[1]}, n_samples);
+	instance->ppm.process({outputs[0], outputs[1]}, {nullptr, nullptr}, n_samples);
+#endif
+}
+
+}

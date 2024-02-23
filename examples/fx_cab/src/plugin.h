4b10a04b3c429b45ab5fc08671ccd2f955db38cd
Stefano D'Angelo <s@dangelo.audio>
2024-02-23 14:17:01 +0100

    copyright notices in examples

--- examples/fx_cab/src/plugin.h
+++ examples/fx_cab/src/plugin.h
@@ -1,3 +1,23 @@
+/*
+ * Brickworks
+ *
+ * Copyright (C) 2023, 2024 Orastron Srl unipersonale
+ *
+ * Brickworks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * Brickworks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Brickworks.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * File author: Stefano D'Angelo
+ */
+
 #include "common.h"
 #include <bw_cab.h>
 
